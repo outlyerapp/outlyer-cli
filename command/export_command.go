0a4fb5804409318d10fe346e34ad2e88d412fb1a
Jorge Acetozi <jorge.acetozi@gmail.com>
2018-07-20 14:31:02 -0300

    Rename checks fields before persisting them to disk

--- command/export_command.go
+++ command/export_command.go
@@ -32,13 +32,13 @@ $ outlyer export alerts dashboards/docker dashboards/kafka --account=<your_accou
 		Run: exportCommand,
 	}
 
-	cmd.PersistentFlags().StringP("account", "a", "", "User account to use")
-	cmd.PersistentFlags().StringP("folder", "f", "", "Folder to save resources")
+	cmd.PersistentFlags().StringP("account", "a", "", "(Required) User account to use")
+	cmd.PersistentFlags().StringP("folder", "f", "", "(Optional) Folder to export resources. If not provided, exports to the current folder")
 	return cmd
 }
 
-// exportCommand fetches the resources from a specific user account
-// and persists them locally.
+// exportCommand validates the user input and calls export for each resource
+// provided by the user
 func exportCommand(cmd *cobra.Command, args []string) {
 	account := cmd.PersistentFlags().Lookup("account").Value.String()
 	if account == "" {
@@ -49,25 +49,25 @@ func exportCommand(cmd *cobra.Command, args []string) {
 		ExitWithError(ExitBadArgs, fmt.Errorf("Resource is required"))
 	}
 
-	baseOutputFolder := getBaseOutputFolder(cmd.PersistentFlags().Lookup("folder").Value.String())
+	outputFolderFlag := cmd.PersistentFlags().Lookup("folder").Value.String()
 
 	// Avoids fetching specific resources if the arguments contain "all"
-	for _, resourceFlagValue := range args {
-		if resourceFlagValue == "all" {
+	for _, resourceToFetch := range args {
+		if resourceToFetch == "all" {
 			fmt.Print("Exporting alerts... ")
-			export("alerts", baseOutputFolder, account)
+			export("alerts", account, getOutputFolder(outputFolderFlag, "alerts"))
 			fmt.Println("Done!")
 
 			fmt.Print("Exporting checks... ")
-			export("checks", baseOutputFolder, account)
+			export("checks", account, getOutputFolder(outputFolderFlag, "checks"))
 			fmt.Println("Done!")
 
 			fmt.Print("Exporting dashboards... ")
-			export("dashboards", baseOutputFolder, account)
+			export("dashboards", account, getOutputFolder(outputFolderFlag, "dashboards"))
 			fmt.Println("Done!")
 
 			fmt.Print("Exporting plugins... ")
-			export("plugins", baseOutputFolder, account)
+			export("plugins", account, getOutputFolder(outputFolderFlag, "plugins"))
 			fmt.Println("Done!")
 
 			ExitWithSuccess("Your account was successfully exported")
@@ -77,60 +77,31 @@ func exportCommand(cmd *cobra.Command, args []string) {
 	// There is no "all" argument, so fetches all listed resources
 	for _, resourceToFetch := range args {
 		fmt.Printf("Exporting %s... ", resourceToFetch)
-		export(resourceToFetch, baseOutputFolder, account)
+		export(resourceToFetch, account, getOutputFolder(outputFolderFlag, resourceToFetch))
 		fmt.Println("Done!")
 	}
 	ExitWithSuccess("Resources successfully exported")
 }
 
-// export queries the Outlyer API for the provided resource(s) based on the user account set
-// and then persists the response to yaml file(s) locally
-func export(resourceToFetch, baseOutputFolder, account string) {
+// export queries the resources for the given user account and persists them locally
+func export(resourceToFetch, account, outputFolder string) {
 	resp, err := outlyer.Get("/accounts/" + account + "/" + resourceToFetch + "?view=export")
 	if err != nil {
 		ExitWithError(ExitError, fmt.Errorf("Could not fetch resource %s from account %s%s", resourceToFetch, account, err))
 	}
 
-	// Builds the correct output folder based on the resource to export
-	var baseResource string
-	slashIndex := strings.Index(resourceToFetch, "/")
-	if slashIndex != -1 {
-		baseResource = resourceToFetch[:slashIndex]
-	} else {
-		baseResource = resourceToFetch
-	}
-	outputFolder := baseOutputFolder + baseResource + "/"
-
-	if strings.Contains(resourceToFetch, "/") {
-		exportSingleResourceToDisk(outputFolder, resp)
-	} else {
-		exportMultipleResourcesToDisk(outputFolder, resp)
-	}
-}
-
-// exportSingleResourceToDisk converts the []byte response to a single yaml file
-// and saves it into the given outputFolder
-func exportSingleResourceToDisk(outputFolder string, resp []byte) {
-	var resource map[string]interface{}
-	yaml.Unmarshal(resp, &resource)
-
-	os.MkdirAll(outputFolder, 0755)
-
-	resourceFileName := outputFolder + strings.Replace(resource["name"].(string), ".py", "", 1) + ".yaml"
-	resourceInBytes, _ := yaml.Marshal(&resource)
-
-	err := ioutil.WriteFile(resourceFileName, resourceInBytes, 0644)
-	if err != nil {
-		ExitWithError(ExitError, fmt.Errorf("Could not write resource %s to disk\n%s", resourceFileName, err))
-	}
-}
-
-// exportMultipleResourcesToDisk converts the []byte response to multiple yaml files
-// and saves them into the given outputFolder
-func exportMultipleResourcesToDisk(outputFolder string, resp []byte) {
 	var resources []map[string]interface{}
-	yaml.Unmarshal(resp, &resources)
 
+	if isSingleResource(resourceToFetch) {
+		var singleResource map[string]interface{}
+		yaml.Unmarshal(resp, &singleResource)
+		resources = make([]map[string]interface{}, 1)
+		resources[0] = singleResource
+	} else {
+		yaml.Unmarshal(resp, &resources)
+	}
+
+	convertCheckFields(resources, outputFolder)
 	os.MkdirAll(outputFolder, 0755)
 
 	for _, resource := range resources {
@@ -144,16 +115,48 @@ func exportMultipleResourcesToDisk(outputFolder string, resp []byte) {
 	}
 }
 
-// getBaseOutputFolder is a helper function to build the correct output base folder name to export resources
-func getBaseOutputFolder(outputFolder string) string {
-	if outputFolder == "" {
-		outputFolder = "./"
+// convertCheckFields converts the format and variables field names into handler and env
+// this function can be removed as soon as https://github.com/outlyerapp/public_api/issues/349
+// is merged.
+func convertCheckFields(checks []map[string]interface{}, outputFolder string) {
+	if strings.Contains(outputFolder, "checks") {
+		for _, check := range checks {
+			handler := check["format"]
+			env := check["variables"]
+
+			check["handler"] = handler
+			check["env"] = env
+
+			delete(check, "format")
+			delete(check, "variables")
+		}
 	}
-	if outputFolder[:1] == "~" {
-		outputFolder = strings.Replace(outputFolder, "~", os.Getenv("HOME"), 1)
+}
+
+// getOutputFolder is a helper function to build the correct output folder to export the given resource
+func getOutputFolder(outputFolderFlag, resourceToFetch string) string {
+	if outputFolderFlag == "" { // Replace empty by the current directory
+		outputFolderFlag = "."
 	}
-	if outputFolder[len(outputFolder)-1:] != "/" {
-		outputFolder = outputFolder + "/"
+	if outputFolderFlag[:1] == "~" { // Replace ~ by the user's full home path
+		outputFolderFlag = strings.Replace(outputFolderFlag, "~", os.Getenv("HOME"), 1)
 	}
+	if outputFolderFlag[len(outputFolderFlag)-1:] != "/" { // Appends a / to the end of the folder path
+		outputFolderFlag = outputFolderFlag + "/"
+	}
+
+	var baseResource string
+	slashIndex := strings.Index(resourceToFetch, "/")
+	if slashIndex != -1 { // The user specified a single resource like 'dashboards/docker', so ignore '/docker'
+		baseResource = resourceToFetch[:slashIndex] + "/"
+	} else {
+		baseResource = resourceToFetch + "/"
+	}
+	outputFolder := outputFolderFlag + baseResource
 	return outputFolder
+}
+
+// isSingleResource checks whether the user provided a single resource like dashboards/docker
+func isSingleResource(resourceToFetch string) bool {
+	return strings.Contains(resourceToFetch, "/")
 }
