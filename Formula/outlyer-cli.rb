65fc530ef2c2560106fe732672c0b80cde406177
Jorge Acetozi <jorge.acetozi@gmail.com>
2018-09-03 14:42:53 -0400

    Updates Formula version

--- Formula/outlyer-cli.rb
+++ Formula/outlyer-cli.rb
@@ -1,9 +1,9 @@
 class OutlyerCli < Formula
   desc "Outlyer CLI allows to easily manage your Outlyer account via command line."
   homepage "https://www.outlyer.com/"
-  url "https://github.com/outlyerapp/outlyer-cli/releases/download/0.2.1/outlyer-cli_0.2.1_Darwin_x86_64.tar.gz"
-  version "0.2.1"
-  sha256 "3bdeaa77a8d62c95dbe440086a6a2704e2018710ccd7b7b72f8f09526dadd690"
+  url "https://github.com/outlyerapp/outlyer-cli/releases/download/1.0.0/outlyer-cli_1.0.0_Darwin_x86_64.tar.gz"
+  version "1.0.0"
+  sha256 "b9435f2909cadb6412faef9488cc26a6618cd7517862a28a1dce62a1cf34f102"
 
   def install
     bin.install "outlyer"
